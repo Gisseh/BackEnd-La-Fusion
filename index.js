bab9545cd72d3d299aed17f97ea76f6e45346d24
Gi Hadad <gihadad89@gmail.com>
2024-07-14 11:51:10 -0300

    cambios

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+require("dotenv").config();
+
+const exp = require("constants");
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("BACKEND F.E");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// const productosRouter = require("./routes/productos.router");
+// app.use("/productos", productosRouter);
+
+app.use("/productos", require("./routes/productos.router"));
+
+app.use("/auth", require("./routes/auth.router"));
+
+app.get("/", (req, res) => {
+    res.send("");
+});
+
+
+
+app.get("/productos/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send("Producto: " + req.params.id);
 });
 
 const PORT = process.env.PORT || 3001;
