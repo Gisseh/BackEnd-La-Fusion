2472537a180fc24e0b5582341048caf9cc489b7f
Gi Hadad <gihadad89@gmail.com>
2024-07-14 14:16:58 -0300

    cambios depend

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // const productosRouter = require("./routes/productos.router");
